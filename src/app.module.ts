2f6293d4e62af6ef2def1ad7e9a9262269d1cdf0
Femi Ayo-Tubosun <femiayotubosun@gmail.com>
2022-04-07 23:53:40 +0100

    Created Order Resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ProductModule } from './product/product.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { UserModule } from './user/user.module';
     }),
     AuthModule,
     UserModule,
+    OrderModule,
   ],
   controllers: [],
   providers: [],
