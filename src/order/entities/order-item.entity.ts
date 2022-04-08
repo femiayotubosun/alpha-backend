de83bc6f8cd6563c33fceccf5754254db6c951dd
Femi Ayo-Tubosun <femiayotubosun@gmail.com>
2022-04-08 20:10:58 +0100

    Order and associates

--- src/order/entities/order-item.entity.ts
+++ src/order/entities/order-item.entity.ts
@@ -1,11 +1,23 @@
+import { IsNumber, IsOptional } from 'class-validator';
 import { Product } from 'src/product/entities/product.entity';
-import { Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  AfterLoad,
+  AfterUpdate,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { Order } from './order.entity';
 
 @Entity()
 export class OrderItem {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
+  quantity: number;
 
   @ManyToOne((_type) => Order, (order) => order.items, { eager: false })
   order: Order;
@@ -15,9 +27,14 @@ export class OrderItem {
   })
   product: Product;
 
-  //   product: Product;
+  @IsNumber()
+  @IsOptional()
+  protected price: number;
 
-  //   quantity
-
-  // total price
+  @AfterLoad()
+  @AfterInsert()
+  @AfterUpdate()
+  generatePrice(): void {
+    this.price = this.product.price * this.quantity;
+  }
 }
--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersRepository } from 'src/auth/users.repository';
+import { AuthModule } from 'src/auth/auth.module';
+import { OrderRepository } from './order.repository';
+import { OrderItemRepository } from './order-item.repository';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      OrderRepository,
+      OrderItemRepository,
+      UsersRepository,
+    ]),
+    AuthModule,
+  ],
+
   controllers: [OrderController],
-  providers: [OrderService]
+  providers: [OrderService],
 })
 export class OrderModule {}
